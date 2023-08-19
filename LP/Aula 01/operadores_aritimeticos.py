00d571b9a75c94d4f58163c805167dbe8de0bba9
Paulo S Pellegrini Jr <67279009+pspellegrini@users.noreply.github.com>
2023-08-19 14:25:46 -0300

    Add files via upload

---
+++ LP/Aula 01/operadores_aritimeticos.py
@@ -0,0 +1,9 @@
+n1 = 2
+n2 = 5
+
+soma = n1 + n2 # 7
+subtracao = n1 - n2 # -3
+divisao = n2 / n1 # 2.5
+divisao_inteira = n2 // n1 # 2
+modulo = n2 % n1 # 1
+exponenciacao = n2**n1 # 25
---
+++ LP/Aula 01/tipos_primivos.py
@@ -0,0 +1,10 @@
+srting = 'texto' # str
+inteiro = 2 # int
+decimal = 4.3 # float
+booleano = True # bool
+
+# Casting
+str()
+int()
+float()
+bool()
