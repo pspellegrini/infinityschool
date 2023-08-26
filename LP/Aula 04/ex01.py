a6b00b01e4e6093c1d828c6825b7dbf595bed235
Paulo S Pellegrini Jr <67279009+pspellegrini@users.noreply.github.com>
2023-08-26 16:36:55 -0300

    Add files via upload

---
+++ LP/Aula 04/ex01.py
@@ -0,0 +1,19 @@
+'''
+Ex01:
+Façam uma contagem de 100 à 0 com o while.
+
+while {{condicao}}:
+    {{instrucoes}}
+Fim do Loop
+'''
+
+#Entrada
+count = 100
+
+#Processamento
+while count >= 0:
+    print(count)
+    count -= 1
+
+#Saída
+print('Fim do Loop.')
